3c43fa9f1b9980528dff07419769dedd0db62b2b
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-08-06 15:27:16 +0200

    Aggiunto alert

--- js/script.js
+++ js/script.js
@@ -5,11 +5,14 @@ var sesso = prompt("Inserisci il tuo sesso: maschio o femmina!!");
 
 var element = document.getElementById("name");
 //colore blu per maschi, colore rosa per femmine in base al sesso inserito
-if(sesso == "maschio" || sesso == "MASCHIO" || sesso == "Maschio"){
+if(sesso === "maschio" || sesso === "MASCHIO" || sesso === "Maschio"){
   element.className = "blue";
   document.getElementById("name").innerHTML = nome;
 }
-  else if(sesso == "femmina" || sesso == "FEMMINA" || sesso == "Femmina"){
+  else if(sesso === "femmina" || sesso === "FEMMINA" || sesso === "Femmina"){
   element.className = "pink";
   document.getElementById('name').innerHTML = nome;
 }
+  else{
+    alert("occhio hai sbagliato a scrivere!!!")
+  }
