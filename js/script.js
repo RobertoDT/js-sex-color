249aeaf3f5d681dfb179869fa6c7d7f2b116b879
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-08-06 15:12:15 +0200

    Esercizio sex-color

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//chiedere il nome all'utente
+var nome = prompt("Inserisci il tuo nome");
+//chiedere il sesso all'utente
+var sesso = prompt("Inserisci il tuo sesso: maschio o femmina!!");
+
+var element = document.getElementById("name");
+//colore blu per maschi, colore rosa per femmine in base al sesso inserito
+if(sesso == "maschio" || sesso == "MASCHIO"){
+  element.className = "blue";
+  document.getElementById("name").innerHTML = nome;
+}
+  else if(sesso == "Maschio"){
+  element.className = "blue";
+  document.getElementById("name").innerHTML = nome;
+}
+  else if(sesso == "femmina" || sesso == "FEMMINA"){
+  element.className = "pink";
+  document.getElementById('name').innerHTML = nome;
+}
+  else if (sesso == "Femmina"){
+  element.className = "pink";
+  document.getElementById('name').innerHTML = nome;
+}
